112c498a66e677a11a5bdd1fc9bc11870087a83b
A186-L <leenametha7@gmail.com>
2026-04-10 20:22:31 +0530

    Add files via upload

---
+++ atm pin.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int pin, a=2208;
+	int i;
+	for(i=1; i<=3;i++){
+		printf("Enter PIN:");
+		scanf("%d",&pin);
+		
+		if(pin==a){
+			printf("the pin is correct on attempt%d\n",i);
+			break;
+		}
+		else{
+			printf("wrong PIN");
+			continue;
+		}
+		return 0;
+	}
+}
