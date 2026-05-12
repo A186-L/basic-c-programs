d01a213269dc46a2122fe998ddd37b3479b12a27
A186-L <leenametha7@gmail.com>
2026-05-12 19:21:00 +0530

    Add files via upload

---
+++ addition of matrix.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+	int a[3][3]={1, 2, 3, 4, 5, 6, 7, 8, 9},b[3][3]={4, 5, 6, 7, 8, 9, 1, 2, 3},c[3][3],i,j;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+		c[i][j] = a[i] [j] + b[i] [j];
+	}
+	
+}
+printf("add the two matrices is:\n");
+	for(i=0;i<3;i++){
+			for(j=0;j<3;j++){
+printf("%3d",c[i] [j]);
+}
+printf("\n");
+}
+return 0;
+}
